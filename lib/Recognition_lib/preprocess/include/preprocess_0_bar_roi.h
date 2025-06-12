2e3c0ae81e89460365bd825e2a677673c6aa4956
KongBoy <s89334roy@yahoo.com.tw>
2025-06-12 10:27:27 +0800

    preprocess_0_bar_roi 大整理, 其實應該要改名成 preprocess_3_a 比較合理, 沒關係先改好裡面程式碼現在確定OK了先存一份 等等來改名

--- lib/Recognition_lib/preprocess/include/preprocess_0_bar_roi.h
+++ lib/Recognition_lib/preprocess/include/preprocess_0_bar_roi.h
@@ -9,5 +9,5 @@ using namespace cv;
 
 #endif // BAR_ROI_H_INCLUDED
 
-void on_Roi(int, void *);
-void bar_Roi(Mat &,string);
+void on_Roi(int, void * data);
+void bar_Roi(Mat & dst_img, string window_name, bool debuging=false);
--- lib/Recognition_lib/preprocess/src/preprocess_0_bar_roi.cpp
+++ lib/Recognition_lib/preprocess/src/preprocess_0_bar_roi.cpp
@@ -7,89 +7,91 @@
 #include <opencv2/imgproc/imgproc.hpp>
 
 #include "preprocess_0_bar_roi.h"
+#include <iostream>
+using namespace std;
 
 ///////////////////////////////////////////////////////
 #define BAR_WINDOW "~ a lot of track bar ~"
 #define BAR_WINDOW_WIDTH 300
 #define BAR_WINDOW_HEIGHT 300
 //////////////////////////////////////////////////////
-#define ROI_ROW_BAR "【ROW】"
-#define ROI_COL_BAR "【COL】"
+#define ROI_ROW_BAR "ROW"
+#define ROI_COL_BAR "COL"
 #define ROI_R_SLIDER_DEFAULT 100
 #define ROI_C_SLIDER_DEFAULT 10
 //////////////////////////////////////////////////////
 #define ENTER 13
 #define ESC 27
-#define SRC_IMG_WINDOW "src_img"
 #define BROWSE_WINDOW "browse_img"
 ///////////////////////////////////////////////////////
 
 using namespace cv;
 
-static Mat src_img;
 static Mat proc_img;
+static bool debuging_roi;
 
 int roi_r_slider = ROI_R_SLIDER_DEFAULT;
 int roi_c_slider = ROI_C_SLIDER_DEFAULT;
 
 
-void on_Roi(int, void *){
-    // Mat proc_img = (*(Mat*)in_data).clone();//如果接local資料，先強制轉換，在"*"，在整個括號才能".??()"
+void on_Roi(int, void * data){
+	Mat* dst_img_ptr = static_cast<Mat*>(data);
+	Mat& dst_img     = *dst_img_ptr;
 
-	// ◎Step：做處理
-	if ((roi_c_slider != 0) && (roi_r_slider != 0) )//&& ( (roi_c_slider != ROI_C_SLIDER_DEFAULT) || (roi_r_slider != ROI_R_SLIDER_DEFAULT) ))
-	{
-		Point2f center(src_img.cols / 2, src_img.rows / 2);
-		float roi_width = src_img.cols * roi_c_slider / 100;
-		float roi_heigh = src_img.rows * roi_r_slider / 100;
+	// 
+	if ((roi_c_slider != 0) && (roi_r_slider != 0) ) {
+		Point2f center(dst_img.cols / 2, dst_img.rows / 2);
+		float roi_width = dst_img.cols * roi_c_slider / 100;
+		float roi_heigh = dst_img.rows * roi_r_slider / 100;
 
-		proc_img = src_img(Rect(center.x - roi_width / 2, center.y - roi_heigh / 2, roi_width, roi_heigh));
-		//cout<<center.x - roi_width / 2<<endl;
-		// 左上 往下延伸
-		// roi_proc_img = ord_img(Rect(0,0,roi_width,roi_heigh));
-		// 左中 往上下延伸
-		// roi_proc_img = ord_img(Rect(0,ord_img.rows/2 - roi_heigh/2,roi_width,roi_heigh));
-		// cout<<"finish roi"<<endl;
-		// *****************************************************************
-		///imshow(BROWSE_WINDOW, proc_img);  //◎Step SHOW-1：做完處理的結果show出來
-
+		proc_img = dst_img(Rect(center.x - roi_width / 2, center.y - roi_heigh / 2, roi_width, roi_heigh));
 	}
-	// *****************************************************************
+	
+	if(debuging_roi){
+		Mat browse_img;
+		resize(proc_img, browse_img, Size(proc_img.cols * (650.0 / proc_img.rows), proc_img.rows * (650.0 / proc_img.rows)));
+		imshow(BROWSE_WINDOW, browse_img);
+		cout << "dst_img   .cols = " << dst_img   .cols << ", dst_img   .rows = " << dst_img   .rows << endl;
+		cout << "proc_img  .cols = " << proc_img  .cols << ", proc_img  .rows = " << proc_img  .rows << endl;
+		cout << "browse_img.cols = " << browse_img.cols << ", browse_img.rows = " << browse_img.rows << endl;
+		cout << endl;
+	}
+
 }
 
-void bar_Roi(Mat & in_src_img , string window_name){
-    namedWindow(SRC_IMG_WINDOW, WINDOW_AUTOSIZE);
-	// moveWindow(SRC_IMG_WINDOW, 0, BAR_WINDOW_HEIGHT + 30);
-	namedWindow(BROWSE_WINDOW, WINDOW_AUTOSIZE);
-	// moveWindow(BROWSE_WINDOW, BAR_WINDOW_WIDTH + 20, 0);
+void bar_Roi(Mat & dst_img , string window_name, bool debuging){
+	debuging_roi = debuging;
 
-    namedWindow(BAR_WINDOW, WINDOW_AUTOSIZE);
-	//resizeWindow(BAR_WINDOW, BAR_WINDOW_WIDTH, BAR_WINDOW_HEIGHT);
-	// moveWindow(BAR_WINDOW, 0, 0);
+	if(debuging_roi){
+		Mat debug_img = dst_img.clone();
+		on_Roi(0, (void *)&debug_img);
+		// 建立 滑桿視窗
+		namedWindow   (BAR_WINDOW, WINDOW_AUTOSIZE);
+		cvMoveWindow  (BAR_WINDOW, 0, 0);
+		cvResizeWindow(BAR_WINDOW, BAR_WINDOW_WIDTH, BAR_WINDOW_HEIGHT);
+		
+		// 建立 預覽視窗
+		namedWindow   (BROWSE_WINDOW , WINDOW_AUTOSIZE);
+		cvMoveWindow  (BROWSE_WINDOW , BAR_WINDOW_WIDTH + 20, 0);
+		
+		// 建立滑桿視窗 和 連結相關 滑桿變數, 滑桿function
+		createTrackbar(ROI_ROW_BAR, BAR_WINDOW, &roi_r_slider, 100, on_Roi, (void *)&debug_img); //如果用local的丟資料，要強制轉換(void*)&ord_img
+		createTrackbar(ROI_COL_BAR, BAR_WINDOW, &roi_c_slider, 100, on_Roi, (void *)&debug_img);
+	
+		// 按 ESC 或 ENTER 結束預覽
+		unsigned char keyboard;
+		do{
+			keyboard = waitKey();
+		}
+		while ( (keyboard != ESC) && (keyboard != ENTER));
+		// 關閉視窗
+		destroyWindow(BAR_WINDOW);
+		destroyWindow(BROWSE_WINDOW);
+	}
 
-	createTrackbar(ROI_ROW_BAR, BAR_WINDOW, &roi_r_slider, 100, on_Roi);//如果用local的丟資料，要強制轉換(void*)&ord_img
-	createTrackbar(ROI_COL_BAR, BAR_WINDOW, &roi_c_slider, 100, on_Roi);
 
-    src_img = in_src_img.clone();
-	// *****************************************************************
-	// imshow(SRC_IMG_WINDOW, src_img);
+	on_Roi(0, (void *)&dst_img);
 
-    proc_img = src_img.clone();
-    on_Roi(0,0);
-
-	// *****************************************************************
-	unsigned char keyboard;
-	do{
-        keyboard = waitKey();
-    }
-	while ( (keyboard != ESC) && (keyboard != ENTER));
-
-	destroyWindow(BAR_WINDOW);
-    destroyWindow(SRC_IMG_WINDOW);
-    destroyWindow(BROWSE_WINDOW);
-
-	in_src_img = proc_img.clone();
-	// ******************************************************
-	// imwrite(window_name + ".bmp",in_src_img);
-	// return;
+	dst_img = proc_img.clone();
+	imwrite("debug_img/pre3_horizontal_line_1_ROI.jpg",dst_img);
 }
