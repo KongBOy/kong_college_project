1fd0887f99093bcec7a0b6bfeb128099eac89fd9
KongBoy <s89334roy@yahoo.com.tw>
2025-06-19 12:49:52 +0800

    preprocess_4 整理 OK

--- lib/Recognition_lib/preprocess/include/preprocess_4_distance_detect.h
+++ lib/Recognition_lib/preprocess/include/preprocess_4_distance_detect.h
@@ -1,9 +1,3 @@
 
 
-void Distance_detect(vector<Vec2f> mountain_lines2 , int *& dist_level);
-#ifndef DISTANCE_DETECT_H_INCLUDED
-#define DISTANCE_DETECT_H_INCLUDED
-
-
-
-#endif // DISTANCE_DETECT_H_INCLUDED
+int * Distance_detect(vector<Vec2f> mountain_lines2, bool debuging);
