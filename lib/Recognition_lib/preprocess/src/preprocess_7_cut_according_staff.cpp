896ba254da9a44e1235c912b8c3ab85e8ab970ce
KongBoy <s89334roy@yahoo.com.tw>
2025-08-05 11:11:32 +0800

    preprocess_7 刪除沒用到的程式碼, 加些註解, padding_U 加些空間給 便是完後標音高的圈圈

--- lib/Recognition_lib/preprocess/src/preprocess_7_cut_according_staff.cpp
+++ lib/Recognition_lib/preprocess/src/preprocess_7_cut_according_staff.cpp
@@ -1,7 +1,7 @@
 /*
 這個程式的功能是：
 利用 preprocess_6 找出的 五線譜端點
-把 輸入譜的圖片 以五線譜為單位做切割(根據 每組五線譜端點 做切割
+把 輸入譜的圖片 以五線譜為單位 盡可能透射成長方形的五線譜 並 做切割(把透射結果放進適當大小的畫布中就有切割的效果囉)
 */
 
 #include <opencv2/core/core_c.h>
@@ -14,22 +14,11 @@
 #include <direct.h>
 #include <iomanip>
 
-
 #include "preprocess_7_cut_according_staff.h"
-
-// #include "vertical_map_to_recognize.h"
-
-
 
 #include "string_tools.h"
 
-#define HORIZONTAL_DIR "horizontal_test/"
-
-#define EXTEND_RATE 1.0
 #define STAFF_LINE_COUNT 5
-#define X_Y_COUNT 2
-
-
 
 using namespace cv;
 using namespace std;
@@ -130,8 +119,8 @@ void Cut_staff(Mat src_bin,Mat src_bin_erase_line,
         int staff_height =   43;
         int padding_L   =    5;
         int padding_R   =    5;
-        int padding_U   =    1.5 * staff_height;
-        int padding_D   =    1.5 * staff_height;
+        int padding_U   =    (1.5 + 0.2) * staff_height;  // 超出五線譜外都可能還有音喔, 所以保險起見多留1.5組五線譜的空間這樣子, 高音的部分還要放辨識出來的音高圈圈, 所以多留 0.2 組五線譜
+        int padding_D   =     1.5        * staff_height;  // 超出五線譜外都可能還有音喔, 所以保險起見多留1.5組五線譜的空間這樣子,
         dst4P[0] = Point2f(       0     + padding_L, padding_U                );
         dst4P[1] = Point2f(       0     + padding_L, padding_U + staff_height );
         dst4P[2] = Point2f( staff_width + padding_L, padding_U                );
@@ -198,138 +187,8 @@ void Cut_staff(Mat src_bin,Mat src_bin_erase_line,
         imwrite(debug_path                , final_img_roi           [staff_num]);
         imwrite(debug_path_erase_line     , final_img_roi_erase_line[staff_num]);
         imwrite(debug_path_reference_line , debug_ref_line);
-
-        // waitKey(0);
-        // imshow("warp_perspective_roi",final_img_roi[staff_num]);
-        // imshow("warp_perspective_rl_roi",final_img_roi_erase_line[staff_num]);
         // waitKey(0);
 
-        // Mat vertical_map(final_img_roi_erase_line[staff_num].rows,
-        //                  final_img_roi_erase_line[staff_num].cols, CV_8UC1, Scalar(255));
-
-        // Vertical_map_to_recognize(final_img_roi_erase_line,vertical_map,final_img_roi,trans[0].y);
-
-        // ***************************************************************************************************
-        // if(staff_num < 4 ) continue;
-
-        /*
-        int l_edge[200];
-        int r_edge[200];
-        int distance[200];
-        int mountain_area[200];
-        int e_count = 0;
-        int* note_type;
-        recognition_0_vertical_map_to_speed_up(final_img_roi_erase_line[staff_num],
-                                               vertical_map,
-                                               e_count,l_edge,r_edge,distance,mountain_area,note_type);
-        */
-
-
-
-        /*
-        // 自己設資料結構 head
-        int maybe_head_count = 0;
-        float maybe_head[3][200];
-        for(int i = 0 ; i < 3 ; i++)
-            for(int j = 0 ; j < 200 ; j++)
-                maybe_head[i][j] = 0;
-
-
-        // 自己設資料結構 line
-        short lines[3][200];///[0]頂點x [1]頂點y [2]長度
-        bool lines_dir[2][200];///[0]左下 [1]右上
-
-        for(int i = 0 ; i < 3 ; i++)
-            for(int j = 0 ; j < 200 ; j++)
-                lines[i][j] = 0;
-
-        for(int i = 0 ; i < 2 ; i++)
-            for(int j = 0 ; j < 200 ; j++)
-                lines_dir[i][j] = false;
-
-        int lines_count = 0;
-
-        int lines_time[200];
-        for(int i = 0 ; i < 200 ; i++) lines_time[i] = 0;
-        */
-        /*
-        // 自己設資料結構 note ~~~~~~~寫出去~~~~~~~~~
-
-        int row_note[5][1000]; /// 0 = x , 1 = y , 2 = type , 3 = time_bar , 4 = 音高
-        int row_note_count = 0;
-        for(int i = 0 ; i < 5 ; i++)
-            for(int j = 0 ; j < 1000 ; j++)
-                row_note[i][j] = 0;
-        */
-
-
-
-        /// ● 0 全音符
-        /// 1 全休止符
-        /// ● 2 二分 音符
-        /// 3 二分 休止符
-        /// ● 4 四分 音符
-        /// ● 5 四分 休止符
-        /*
-        Mat template_img_4 = imread("Resource/note/4/4.bmp",0);
-        Mat template_img_4_rest = imread("Resource/note/4-rest/4-rest.bmp",0);
-        Mat template_img_2 = imread("Resource/note/2/2.bmp",0);
-        Mat template_img_0 = imread("Resource/note/0/0.bmp",0);
-        Mat template_img_1 = imread("Resource/note/0-rest/0-rest.bmp",0);
-        Mat template_img_3 = imread("Resource/note/2-rest/2-rest.bmp",0);
-        Mat template_img_8 = imread("Resource/note/8-rest/8-rest-3.bmp",0);
-        Mat template_img_6 = imread("Resource/note/6-rest/6-rest-2.bmp",0);
-
-        recognition_0_all_head(1,template_img_1,final_img_roi_erase_line[staff_num],final_img_roi[staff_num],e_count,l_edge,distance,trans[0].y,row_note_count,row_note);
-        recognition_0_all_head(3,template_img_3,final_img_roi_erase_line[staff_num],final_img_roi[staff_num],e_count,l_edge,distance,trans[0].y,row_note_count,row_note);
-        */
-        /*
-        recognition_0_all_head(4,template_img_4,final_img_roi_erase_line[staff_num],final_img_roi[staff_num],e_count,l_edge,distance,trans[0].y,row_note_count,row_note);
-        recognition_0_all_head(2,template_img_2,final_img_roi_erase_line[staff_num],final_img_roi[staff_num],e_count,l_edge,distance,trans[0].y,row_note_count,row_note);
-        recognition_0_all_head(0,template_img_0,final_img_roi_erase_line[staff_num],final_img_roi[staff_num],e_count,l_edge,distance,trans[0].y,row_note_count,row_note);
-        recognition_0_all_head(5,template_img_4_rest,final_img_roi_erase_line[staff_num],final_img_roi[staff_num],e_count,l_edge,distance,trans[0].y,row_note_count,row_note);
-
-
-        recognition_0_all_head(8,template_img_8,final_img_roi_erase_line[staff_num],final_img_roi[staff_num],e_count,l_edge,distance,trans[0].y,row_note_count,row_note);
-        recognition_0_all_head(6,template_img_6,final_img_roi_erase_line[staff_num],final_img_roi[staff_num],e_count,l_edge,distance,trans[0].y,row_note_count,row_note);
-        */
-        // recognition_0_all_head(7,template_img_8,final_img_roi_erase_line[staff_num],final_img_roi[staff_num],e_count,l_edge,distance,trans[0].y,row_note_count,row_note);
-
-
-
-        /*
-        bubbleSort_note(row_note_count,row_note,Y_INDEX);
-        bubbleSort_note(row_note_count,row_note,X_INDEX);
-
-        recognition_5_find_pitch(final_img_roi[staff_num],Mat(15,15,CV_8UC1),row_note_count,row_note,trans[0].y);
-
-
-
-        // midi(row_note_count,row_note);
-
-        cout << endl << endl;
-        cout << "print row_note~~~" << endl;
-        for(int go_row_note = 0 ; go_row_note < row_note_count ; go_row_note++){
-            cout << "go_row_note = " << go_row_note
-                <<" , type = " << row_note[2][go_row_note]
-                <<" , x = " << row_note[0][go_row_note]
-                <<" , y = " << row_note[1][go_row_note]
-                <<" , time = " << row_note[3][go_row_note]
-                <<" , pitch = " << row_note[4][go_row_note]
-                <<endl;
-        }
-        cout << "row_note_count = " << row_note_count << endl;
-
-        for(int go_row_note = 0 ; go_row_note < row_note_count ; go_row_note++){
-            note[0][note_count+go_row_note] = row_note[0][go_row_note];
-            note[1][note_count+go_row_note] = row_note[1][go_row_note];
-            note[2][note_count+go_row_note] = row_note[2][go_row_note];
-            note[3][note_count+go_row_note] = row_note[3][go_row_note];
-            note[4][note_count+go_row_note] = row_note[4][go_row_note];
-        }
-        note_count += row_note_count;
-        // play();
-        */
     }
 
 }
