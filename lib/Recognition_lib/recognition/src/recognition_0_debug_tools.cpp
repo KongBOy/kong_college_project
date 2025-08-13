80b92492aa819f232ee37a7591b39048ba75c314
KongBoy <s89334roy@yahoo.com.tw>
2025-08-13 08:31:01 +0800

    recognition_0_debug_tools 排版

--- lib/Recognition_lib/recognition/src/recognition_0_debug_tools.cpp
+++ lib/Recognition_lib/recognition/src/recognition_0_debug_tools.cpp
@@ -29,73 +29,62 @@ static Mat template_img_6 = imread("Resource/note/6-rest/6-rest-2.bmp",0);
 static Mat template_img_7 = imread("Resource/note/32-rest/7-2.bmp",0);
 */
 
-void list_lines_info(int lines_count,short lines[][200],bool lines_dir[][200])
-{
-    cout<<"lines_count = "<<lines_count<<endl;
+void list_lines_info(int lines_count,short lines[][200],bool lines_dir[][200]){
+    cout << "lines_count = " << lines_count<<endl;
     for(int go_line = 0 ; go_line < lines_count ; go_line++)
-        cout<<"go_line = "<<go_line
-            <<" , x = "<<lines[0][go_line]
-            <<" , y = "<<lines[1][go_line]
-            <<" , length = "<<lines[2][go_line]
-            <<" , down = "<<lines_dir[0][go_line]
-            <<" , top = "<<lines_dir[1][go_line]<<endl;
-    cout<<endl;
+        cout << "go_line = "   << go_line
+             << " , x = "      << lines[0][go_line]
+             << " , y = "      << lines[1][go_line]
+             << " , length = " << lines[2][go_line]
+             << " , down = "   << lines_dir[0][go_line]
+             << " , top = "    << lines_dir[1][go_line] << endl;
+    cout << endl;
 }
 
-void draw_lines(Mat& debug_img,int lines_count,short lines[][200],bool lines_dir[][200])
-{
-/// 顯示 線
-    for(int go_line = 0 ; go_line < lines_count ; go_line++)
-    {
-        /// 左
-        if(lines_dir[0][go_line] == true && lines_dir[1][go_line] == false)
-        {
+void draw_lines(Mat& debug_img,int lines_count,short lines[][200],bool lines_dir[][200]){
+    // 顯示 線
+    for(int go_line = 0 ; go_line < lines_count ; go_line++){
+        // 左
+        if(lines_dir[0][go_line] == true && lines_dir[1][go_line] == false){
             line(debug_img, Point(lines[0][go_line],lines[1][go_line]),Point(lines[0][go_line],lines[1][go_line]+lines[2][go_line]),Scalar(123,255,123),2 );
         }
-        /// 右
-        else if(lines_dir[0][go_line] == false && lines_dir[1][go_line] == true)
-        {
+        // 右
+        else if(lines_dir[0][go_line] == false && lines_dir[1][go_line] == true){
             line(debug_img, Point(lines[0][go_line],lines[1][go_line]),Point(lines[0][go_line],lines[1][go_line]+lines[2][go_line]),Scalar(23,255,223),2 );
         }
-        /// 右+左
-        else if((lines_dir[0][go_line] == true && lines_dir[1][go_line] == true) || (lines_dir[0][go_line] == false && lines_dir[1][go_line] == false))
-        {
+        // 右+左
+        else if((lines_dir[0][go_line] == true && lines_dir[1][go_line] == true) || (lines_dir[0][go_line] == false && lines_dir[1][go_line] == false)){
             line(debug_img, Point(lines[0][go_line],lines[1][go_line]),Point(lines[0][go_line],lines[1][go_line]+lines[2][go_line]),Scalar(0,0,255),2 );
         }
 
-        ///cout<<"go_line = "<<go_line<<" , x = "<<lines[0][go_line]<<" , y = "<<lines[1][go_line]<<" , length = "<<lines[2][go_line]<<" , left = "<<lines_dir[0][go_line]<<" , right = "<<lines_dir[1][go_line]<<endl;
-        ///imshow("lines",debug_img);
+        // cout<<"go_line = "<<go_line<<" , x = "<<lines[0][go_line]<<" , y = "<<lines[1][go_line]<<" , length = "<<lines[2][go_line]<<" , left = "<<lines_dir[0][go_line]<<" , right = "<<lines_dir[1][go_line]<<endl;
+        // imshow("lines",debug_img);
 
-        ///waitKey(0); ///一條一條慢慢看
+        // waitKey(0); ///一條一條慢慢看
     }
 }
 
 
-void watch_lines(Mat debug_img,int lines_count,short lines[][200],bool lines_dir[][200])
-{
-/// 顯示 線
-    for(int go_line = 0 ; go_line < lines_count ; go_line++)
-    {
-        /// 左
-        if(lines_dir[0][go_line] == true && lines_dir[1][go_line] == false)
-        {
+void watch_lines(Mat debug_img,int lines_count,short lines[][200],bool lines_dir[][200]){
+    // 顯示 線
+    for(int go_line = 0 ; go_line < lines_count ; go_line++){
+        // 左
+        if(lines_dir[0][go_line] == true && lines_dir[1][go_line] == false){
             line(debug_img, Point(lines[0][go_line],lines[1][go_line]),Point(lines[0][go_line],lines[1][go_line]+lines[2][go_line]),Scalar(123,255,123),2 );
         }
-        /// 右
-        else if(lines_dir[0][go_line] == false && lines_dir[1][go_line] == true)
-        {
+        // 右
+        else if(lines_dir[0][go_line] == false && lines_dir[1][go_line] == true){
             line(debug_img, Point(lines[0][go_line],lines[1][go_line]),Point(lines[0][go_line],lines[1][go_line]+lines[2][go_line]),Scalar(23,255,223),2 );
         }
-        /// 右+左
-        else if((lines_dir[0][go_line] == true && lines_dir[1][go_line] == true) || (lines_dir[0][go_line] == false && lines_dir[1][go_line] == false))
-        {
+        // 右+左
+        else if((lines_dir[0][go_line] == true && lines_dir[1][go_line] == true) || (lines_dir[0][go_line] == false && lines_dir[1][go_line] == false)){
             line(debug_img, Point(lines[0][go_line],lines[1][go_line]),Point(lines[0][go_line],lines[1][go_line]+lines[2][go_line]),Scalar(0,0,255),2 );
         }
 
-        ///cout<<"go_line = "<<go_line<<" , x = "<<lines[0][go_line]<<" , y = "<<lines[1][go_line]<<" , length = "<<lines[2][go_line]<<" , left = "<<lines_dir[0][go_line]<<" , right = "<<lines_dir[1][go_line]<<endl;
-        ///imshow("lines",debug_img);
+        // cout<<"go_line = "<<go_line<<" , x = "<<lines[0][go_line]<<" , y = "<<lines[1][go_line]<<" , length = "<<lines[2][go_line]<<" , left = "<<lines_dir[0][go_line]<<" , right = "<<lines_dir[1][go_line]<<endl;
+        // imshow("lines",debug_img);
 
-        ///waitKey(0); ///一條一條慢慢看
+        waitKey(0); ///一條一條慢慢看
     }
 
     imshow("watch_lines()",debug_img);
