99415e824b56a335342cdf44a4548c5165239de7
KongBoy <s89334roy@yahoo.com.tw>
2025-08-27 09:20:05 +0800

    recognition_3c 把一些會用到的變數先宣告出來增加可讀性, 加入一些註解, 一些cout註解拿掉

--- lib/Recognition_lib/recognition/src/recognition_3_c_merge_head_and_time.cpp
+++ lib/Recognition_lib/recognition/src/recognition_3_c_merge_head_and_time.cpp
@@ -27,15 +27,25 @@ void recognition_4_merge_head_and_time(int head_type,
     int go_bar = 0;
     // cout註解 確認一下想像中的range對不對
     // cout << "((template_img.cols + dist_error) * -1) = " << ((template_img.cols + dist_error) * -1) << endl;
-
+    int maybe_head_x;
+    int maybe_head_y;
+    int bar_x;
+    int bar_y;
+    int bar_len;
+    int distance_x;
     for(int go_head = 0; go_head < maybe_head_count; go_head++){
+        maybe_head_x = maybe_head[0][go_head];
+        maybe_head_y = maybe_head[1][go_head];
+        bar_x = bars[0][go_bar];
+        bar_y = bars[1][go_bar];
+        bar_len = bars[2][go_bar];
         // cout註解 看一下頭的資訊
-        // cout << "maybe_head[0][" << go_head << "] = " << maybe_head[0][go_head]
-        //      << ", bars[0]["    << go_bar << "] = " << bars[0][go_bar]
-        //      << ", value = "     << maybe_head[0][go_head] - bars[0][go_bar];
+        // cout << "maybe_head[0][" << go_head << "] = " << maybe_head_x
+        //      << ", bars[0]["     << go_bar  << "] = " << bar_x
+        //      << ", value = "     << maybe_head_x - bar_x;
 
-        rectangle(debug_img, Point(maybe_head[0][go_head], maybe_head[1][go_head]), Point(maybe_head[0][go_head] + template_img.cols, maybe_head[1][go_head] +template_img.rows), Scalar(0, 255, 0), 2);
-        line( debug_img, Point(bars[0][go_bar], bars[1][go_bar]), Point(bars[0][go_bar], bars[1][go_bar] + bars[2][go_bar]), Scalar(100, 200, 0), 2 );
+        rectangle(debug_img, Point(maybe_head_x, maybe_head_y), Point(maybe_head_x + template_img.cols, maybe_head_y +template_img.rows), Scalar(0, 255, 0), 2);
+        line( debug_img, Point(bar_x, bar_y), Point(bar_x, bar_y + bar_len), Scalar(100, 200, 0), 2 );
 
         // ******************************************
         // ~~~~~~ debug 用來看合併的過程~~~~~~~~~~
@@ -44,42 +54,43 @@ void recognition_4_merge_head_and_time(int head_type,
 
         // check有沒有 線(time_bar)
 
-        int distance = maybe_head[0][go_head] - bars[0][go_bar]; // 值代表大小，+-代表方向，-往左，+往右
+        // bar到head 的x向量, 值代表大小，+-代表方向，-往左，+往右
+        distance_x = maybe_head_x - bar_x;
         // 左邊的離線太遠 不抓線，直接存
-        if((maybe_head[0][go_head] - bars[0][go_bar]) < ((template_img.cols + dist_error) * -1) ){
-            // cout註解 標記現在在哪個case
-            // cout << ", case 1";
-            note[0][note_count] =  maybe_head[0][go_head];
-            note[1][note_count] =  maybe_head[1][go_head];
+        if((distance_x) < ((template_img.cols + dist_error) * -1) ){
+            cout << ", case 1";
+            note[0][note_count] =  maybe_head_x;
+            note[1][note_count] =  maybe_head_y;
             note[2][note_count] =  head_type;
             note[3][note_count] =  0; // 不抓線
             note_count++;
-            //note[4][note_count] = ;
         }
         // 和線夠近 抓 同一條 線
-        else if( ((maybe_head[0][go_head] - bars[0][go_bar]) >= ((template_img.cols + dist_error) * -1)) &&
-                 ((maybe_head[0][go_head] - bars[0][go_bar]) <= (0 + dist_error) ) ){
-            // cout註解 標記現在在哪個case
-            // cout << ", case 2";
+        else if( ((distance_x) >= ((template_img.cols + dist_error) * -1)) &&
+                 ((distance_x) <= (0 + dist_error) ) ){
+            cout << ", case 2";
 
             // 如果是很擠的狀況~~就非常尷尬拉~~要看他是和 此條線比較靠近() 還是和 下一條線比較接近
-            if( distance >= 0 && distance <= (0 + dist_error) &&
+            //  |
+            //  | 目前在這條bar
+            // *  
+            //    * 目前在這顆頭, 
+            //   | 這條目前是 next_bar
+            //   |
+            if( distance_x >= 0 && distance_x <= (0 + dist_error) &&
                 bars_dir[go_bar] == false ){
                 if(go_bar >= bars_count-1);  // 如果是在最後一條線出現此case的話，就一定是併排的那種拉，防呆 ~~~do nothing~~~
                 else{
-                    // cout註解 標記現在在哪個case
                     // cout << endl << "embarass~~case " << endl;
 
-                    int distance_next = maybe_head[0][go_head] - bars[0][go_bar+1];
+                    int distance_next = maybe_head_x - bars[0][go_bar+1];
                     distance_next += template_img.cols;
-                    // cout註解 看一下想像中的range對不對
-                    // cout << "distance = " << distance  << ", distance_next = " << distance_next;
+                    // cout << "distance_x = " << distance_x  << ", distance_next = " << distance_next;
 
-                    if(abs( distance) < abs(distance_next));// 現在的頭 和 現在這條線比較近，就繼續做case2~~
-                    else{ // (abs( distance) >= abs(distance_next)) 現在的頭 和 下一條線比較接近，做case3換下一條線拉！
+                    if(abs( distance_x) < abs(distance_next));// 現在的頭 和 現在這條線比較近，就繼續做case2~~
+                    else{ // (abs( distance_x) >= abs(distance_next)) 現在的頭 和 下一條線比較接近，做case3換下一條線拉！
                     
-                        // cout註解 標記現在在哪個case
-                        // cout << ", case 3";
+                        cout << ", case 3";
                         go_bar++;
                         go_head--;
                         cout << endl;
@@ -88,17 +99,15 @@ void recognition_4_merge_head_and_time(int head_type,
                 }
             }
             // ***************************************************************************
-            note[0][note_count] =  maybe_head[0][go_head];
-            note[1][note_count] =  maybe_head[1][go_head];
+            note[0][note_count] =  maybe_head_x;
+            note[1][note_count] =  maybe_head_y;
             note[2][note_count] =  head_type;
             note[3][note_count] =  bars_time[go_bar]; // 抓同條線
             note_count++;
         }
         // 右邊的離線太遠  換 下一條線 ， 這顆頭 換了下一條線 後 需要再跟 這條新的線 比一次，所以head--
-        else{ // 即if((maybe_head[0][go_head] - bars[0][go_bar] > (0 + dist_error))
-        
-            // cout註解 標記現在在哪個case
-            // cout << ", case 3";
+        else{ // 即if((distance_x > (0 + dist_error))
+            cout << ", case 3";
             go_bar++;
             go_head--;
         }
