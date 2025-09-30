db1046f7b5851b6ad91e4e71fa5ac132bcd5372b
KongBoy <s89334roy@yahoo.com.tw>
2025-09-30 16:08:08 +0800

    recognition_0_debug_tools 的 list_bars_info 改名成 list_Bars_infos

--- lib/Recognition_lib/recognition/include/recognition_0_debug_tools.h
+++ lib/Recognition_lib/recognition/include/recognition_0_debug_tools.h
@@ -9,6 +9,6 @@
 using namespace cv;
 
 
-void list_bars_info(int bars_count,short bars[][200],bool bars_dir[200]);
+void list_Bars_infos(int bars_count,short bars[][200],bool bars_dir[200]);
 void draw_bars(Mat& debug_img,int bars_count,short bars[][200],bool bars_dir[200]);
 void watch_bars(Mat debug_img,int bars_count,short bars[][200],bool bars_dir[200]);
