3062c2325b1f2d24383d8775f5d5afb9ca408f8d
KongBoy <s89334roy@yahoo.com.tw>
2025-10-15 16:55:54 +0800

    preprocess_5 大整理OK

--- lib/Recognition_lib/preprocess/include/preprocess_5_find_staff.h
+++ lib/Recognition_lib/preprocess/include/preprocess_5_find_staff.h
@@ -5,17 +5,6 @@ using namespace cv;
 #ifndef FIND_STAFF_H_INCLUDED
 #define FIND_STAFF_H_INCLUDED
 
-
-
 #endif // FIND_STAFF_H_INCLUDED
 
-
-
-void filter_multi_same_line(vector<Vec2f>& src_lines,int, bool debuging);
-void position_erase(vector<Vec2f>&src_lines,int position);
-
-
-//new things
-//不要用vector<Vec2f>*& staff，原因寫在find_staff.cpp
 int find_Staff_lines(vector<Vec2f>& select_lines, int dist_level_0, int dist_level_1, bool debuging=false);
-
