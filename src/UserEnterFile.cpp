31b907d515555e662d8fdbabe8714dd93420b39a
KongBoy <s89334roy@yahoo.com.tw>
2025-10-31 14:48:52 +0800

    UserEnterFile UI整理, while條件整理, 找出最符合想要顏色的點整理, 加點註解, Detect_Volumn 從 DetectHandShakeSpeed 分離出來

--- src/UserEnterFile.cpp
+++ src/UserEnterFile.cpp
@@ -82,9 +82,9 @@ float shaklowest = 0;
 float shakrange = 0;
 
 
-int a = 0;
+// int go_buffer = 0;
 int compute_speed_M[6] = {0};
-int compute_volume_M[6] = {0};
+// int compute_volume_M[6] = {0};
 int average_speed = 0;
 int sum_handY = 0;
 int average_volume = 0;
@@ -103,6 +103,44 @@ CvCapture *  capture ;
 time_t preHandBitClock = clock() + 10000;
 time_t nowHandBitClock = clock() + 10000;
 
+
+void Detect_Volumn(Mat ui_screen, int orbitX[], int orbitY[], int go_orbit, int orbit_num){
+    float len_max = sqrt( pow(ui_screen.rows, 2) + pow(ui_screen.cols, 2) );
+    // 看三條線
+    float orbit_len_acc = 0;
+    float orbit_len_avg = 0;
+    float orbit_len     = 0;
+    int buffer_size = 7;
+    int acc_amount = 0;
+    for(int i = 0; i < buffer_size ; i++ ){
+        int cur_i = go_orbit - i;
+        int bef_i = go_orbit - i - 1;
+        if(cur_i < 0) cur_i = orbit_num + cur_i;
+        if(bef_i < 0) bef_i = orbit_num + bef_i;
+        orbit_len  = pow(orbitX[cur_i] - orbitX[bef_i], 2);
+        orbit_len += pow(orbitY[cur_i] - orbitY[bef_i], 2);
+        orbit_len  = sqrt(orbit_len);
+        if(orbit_len > 20)
+            orbit_len_acc += orbit_len;
+            acc_amount++;
+        // cout << "orbit_len:" << orbit_len << endl;
+        // cout << "go_orbit:" << go_orbit <<  ", cur_i:" << cur_i << ", bef_i:" << bef_i << endl;
+        // line( ui_screen, Point(orbitX[cur_i], orbitY[cur_i]), Point(orbitX[bef_i], orbitY[bef_i]), Scalar(44, 250, 255), 1, 8 );
+    }
+    // cv::imshow("ui_screen", ui_screen);
+    if(acc_amount > 0){
+        orbit_len_avg = orbit_len_acc / acc_amount;
+        float len_ratio = orbit_len_avg / (len_max * 0.060) * 100;
+        // cout << "    len_ratio:" << len_ratio << endl;
+        float temp_volume = len_ratio;       
+        if(temp_volume <   5) temp_volume =   5;  // 最小音量用 10%
+        if(temp_volume > 100) temp_volume = 100;  // 如果超過 100% 就clip掉回100%
+        volume = temp_volume / 100 * 127;         // 0 ~ 100 range 縮放成 MIDI音量 0 ~ 127
+    }
+    
+    // cout << "orbit_len_avg:" << orbit_len_avg << ", len_max:" << len_max << ", temp_volume:" << temp_volume << ", volume:" << volume << endl;
+}
+
 int HandShaking(string Title){
     // time_t LastTimeFineUser     = clock();
     // time_t OverTimeUserDisapper = clock()  +  UserGoOutWhenPlayingTime;
@@ -110,7 +148,7 @@ int HandShaking(string Title){
 	IplImage *  vframe ;
 
 	Mat copyFrame;
-	Mat image;
+	Mat newimg;
 	Mat frame;
 
 	double MinValue;
@@ -124,85 +162,74 @@ int HandShaking(string Title){
 	// if( !face_cascade.load( face_cascade_name ) ){ printf("--(!)Error loading\n"); return -1; };
 
     // 開始視訊
+    SamplePicInitial();  // 初始化 MeanScalar 和 StandardDeviationScalar
+    Mat sample_color;
 	capture = cvCaptureFromCAM( -1 );
+    if(capture){
+        frame = cvQueryFrame( capture );
+        sample_color = Mat( int(frame.rows / 2), int(frame.cols / 2), CV_8UC3, Scalar(MeanScalar.val[0], MeanScalar.val[1], MeanScalar.val[2]));
+    }
 
-    int index = 0;
+    int go_orbit = 0;
 	int orbit_num = 8;
 	int orbitX[8] = {0};
 	int orbitY[8] = {0};
 
     int talktime = 0;
     Mat talk;
+    Mat talk_roi_ord = Output(Rect(700, 130, T1.cols * 0.7, T1.rows * 0.7)).clone();
+    Mat talk_roi     = Output(Rect(700, 130, T1.cols * 0.7, T1.rows * 0.7));
     Mat Output2;
     Mat frame_show;
+
 	// 偵測手環是否進入範圍內
 	if(capture){
-	    SamplePicInitial();
 	    Mat ReducePic;
         Mat ReducePicShow;
         int returnval = 1;
-		while( LastTimeFineUser < OverTimeUserDisapper && returnval == 1){
+        int go_frame = 0;
+		while( MusicPlayback){
 		    // cout << "APPNOWTIME" << LastTimeFineUser << endl;
             // cout << "ENDSTEPTIME" << OverTimeUserDisapper << endl;
 			frame = cvQueryFrame( capture );
 
 			if( !frame.empty() ){
 			  // printf("OK!!!!\n");
-			}else{
-			  printf(" --(!) No captured frame -- Break!"); break;
+			}
+            else{
+			    printf(" --(!) No captured frame -- Break!"); break;
 			}
 
-            // delay 10 毫秒 抓一次圖
+            // delay 1 毫秒 抓一次圖
 			int c = waitKey(1);
 			if( (char)c ==  'c' ) { break; }
 
 
-			image = frame;
-
 			// 臉部偵測的function
-			resize(frame, ReducePic, Size(frame.cols / 3, frame.rows / 3), 0, 0, INTER_CUBIC);
+			resize(frame, newimg, Size(int(frame.cols / 2), int(frame.rows / 2)), 0, 0, INTER_CUBIC);
+            // cv::imshow("newimg", newimg);
+            // cv::waitKey(0);
 
-            // imshow("frame", frame);
-
-
-
-
-            Mat newimg;
-			newimg = ReducePic.clone();
-			Mat probimg(newimg.rows, newimg.cols, CV_8UC1);
-			float B;
-            float G;
-            float R;
-            float stander_dev;
+            // 偵測 顏色最相近的點 在哪裡
             int MinRow = 0;
             int MinCol = 0;
             int MinValue = 300;
-            for(int i = 0; i < newimg.rows; i ++ ){
-                for(int j = 0; j < newimg.cols; j ++ ){
 
-                    B = newimg.at<Vec3b>(i, j)[0];
-                    G = newimg.at<Vec3b>(i, j)[1];
-                    R = newimg.at<Vec3b>(i, j)[2];
-                    stander_dev = sqrt(pow((B-MeanScalar.val[0]), 2) + pow((G-MeanScalar.val[1]), 2) + pow((R-MeanScalar.val[2]), 2));
-                    if(stander_dev<MinValue){
-                        MinValue = stander_dev;
-                        MinRow = i;
-                        MinCol = j;
-                    }
-                    // cout << "stander_dev " << stander_dev << endl;
-                }
-            }
-            // cout << "MinVale " << MinValue << endl;
+            Mat distance;
+            Mat sample_color_f;
+            Mat newimg_f;
+            newimg      .convertTo(newimg_f      , CV_32F);
+            sample_color.convertTo(sample_color_f, CV_32F);
+            pow(newimg_f - sample_color_f,  2.0, distance);
+            vector<Mat> channels;
+            split(distance, channels);
+            distance = channels[0] + channels[1] + channels[2];
+            sqrt(distance, distance);
+            double minVal; double maxVal; Point minLoc; Point maxLoc;
+            minMaxLoc( distance , &minVal, &maxVal, &minLoc, &maxLoc);
+            MinCol = minLoc.x;
+            MinRow = minLoc.y;
 
-            // if(detectAndDisplay(newimg, &facex, &facey, &facewidth, &faceheight)){
-
-            //     OverTimeUserDisapper = clock() + UserGoOutWhenPlayingTime;
-            // }
-
-            // LastTimeFineUser = clock();
-            
-            OverTimeUserDisapper = clock() + UserGoOutWhenPlayingTime;
-            LastTimeFineUser = clock();
 
             nowx = MinCol;
             nowy = MinRow;
@@ -218,43 +245,56 @@ int HandShaking(string Title){
             prex = MinCol;
             prey = MinRow;
 
+            // 畫出 最新偵測到的 顏色位置 orbitXY, 用 藍色圈圈 表示
             circle(newimg, cvPoint(MinCol, MinRow), 4, Scalar(250, 0, 0), 2, 8, 0);
-            if(index < orbit_num){
-				orbitX[index] = MinCol;
-				orbitY[index] = MinRow;
-				index ++ ;
-			}
-            else {
-				index  = 0;
-				orbitX[index] = MinCol;
-				orbitY[index] = MinRow;
-				index ++ ;
-			}
 
+            // 軌跡buffer 存入 顏色最相近的點
+            orbitX[go_orbit] = MinCol;
+            orbitY[go_orbit] = MinRow;
 
+            Detect_Volumn(newimg, orbitX, orbitY, go_orbit, orbit_num);
 
-			for(int i = 0;i < orbit_num;i ++ ){
-                // newimg.at<Vec3b>(orbitY[i], orbitX[i])[0] = 44;
-                // newimg.at<Vec3b>(orbitY[i], orbitX[i])[1] = 250;
-                // newimg.at<Vec3b>(orbitY[i], orbitX[i])[2] = 3;
-                line( newimg, Point(orbitX[i], orbitY[i]), Point(orbitX[(i + 1) % 8], orbitY[(i + 1) % 8]), Scalar(44, 250, 3), 1, 8 );
+            // 畫出 orbit 裡面的點 連成的 線, 最後一個點不用連回頭所以-1
+			for(int i = 0; i < orbit_num - 1 ; i++ ){
+                int cur_i = go_orbit - i;
+                int bef_i = go_orbit - i - 1;
+                if(cur_i < 0) cur_i = orbit_num + cur_i;
+                if(bef_i < 0) bef_i = orbit_num + bef_i;
+
+                // cout << "go_orbit:" << go_orbit <<  ", cur_i:" << cur_i << ", bef_i:" << bef_i << endl;
+                
+                line( newimg, Point(orbitX[cur_i], orbitY[cur_i]), Point(orbitX[bef_i], orbitY[bef_i]), Scalar(44, 250, 3), 1, 8 );
+                // line( newimg, Point(orbitX[i], orbitY[i]), Point(orbitX[(i + 1) % 8], orbitY[(i + 1) % 8]), Scalar(44, 250, 3), 1, 8 );
 				// circle(newimg, cvPoint(orbitX[i], orbitY[i]), 2, Scalar(44, 250, 3), 2, 8, 0);
 			}
-            resize(newimg, newimg, Size(frame.cols * 0.537, frame.rows * 0.537), 0, 0, INTER_CUBIC);
-            // imshow("ReducePic", ReducePicShow);
-            if(!Output.empty()){
-                frame_show = Output(Rect(16, 77, newimg.cols, newimg.rows));
-                newimg.copyTo(frame_show);
-                // resize(row_proc_img[row_index], row_proc_img[row_index], Size(1211, 227), 0, 0, INTER_CUBIC);
-                if(row_proc_img[row_index].rows>227)
-                    DrawMat(row_proc_img[row_index](Rect(0, 0, 1166, 227)), Output, 530, 62);
-                else
-                    DrawMat(row_proc_img[row_index], Output, 530, 62);
 
+            // 更新 軌跡buffer的index
+            if(go_orbit < orbit_num-1) go_orbit++  ;
+            else                       go_orbit = 0;
+
+            // 貼到UI前 先縮小到UI指定的大小
+            resize(newimg, newimg, Size(frame.cols * 0.537, frame.rows * 0.537), 0, 0, INTER_CUBIC);
+
+            // 把東西貼上 UI Output(寫在 Generate_Play_Midi 跟 PlaySnd共用)
+            if(!Output.empty()){
+                
+                // 把 畫完圖的frame 貼上 Output
+                frame_show = Output(Rect(16, 77, newimg.cols, newimg.rows));
+                cv::flip(newimg, newimg, 1);  // 左右翻轉
+                newimg.copyTo(frame_show);
+
+                // 把 五線譜組 貼上 Output                
+                int roi_height = row_proc_img[row_index].rows;
+                int roi_width  = row_proc_img[row_index].cols;
+                if(roi_height > 227) roi_height = 227;  // 高度最多抓 227
+                Mat ui_staff_roi    = Output                 (Rect(62, 530, roi_width, roi_height));
+                Mat staff_staff_roi = row_proc_img[row_index](Rect( 0,   0, roi_width, roi_height));
+                staff_staff_roi.copyTo(ui_staff_roi);
             }
 
+            // 
             Output2 = Output.clone();
-            if(clock()>talktime){
+            if(clock() > talktime){
                 talktime = clock() + 5000 + (rand() % 10 * 1000);
                 switch(1 + rand() % 11){
                 case 1:
@@ -295,10 +335,13 @@ int HandShaking(string Title){
                 // cout << "talktime" << talktime << endl;
                 // cout << "clock()" << clock() << endl;
             }
+            
+            
+            talk_roi_ord.copyTo(talk_roi);     // 先把上次的結果還原回原始UI
+            DrawTalk(talk, Output, 130, 700);  // 再貼上新的Talk圖片
 
-            DrawTalk(talk, Output2, 130, 700);
-
-            imshow(Title, Output2);
+            imshow(Title, Output);
+            go_frame++;
 		}
 		cvReleaseCapture(&capture);
         return returnval;
@@ -361,7 +404,8 @@ void SamplePicInitial(){
     // 舊版 opencv 寫法, 要搭配 cvShowImage, cvWaitKey 才可以顯示喔
     // IplImage* src = cvLoadImage("testmode4.jpg", 1);
     // IplImage* src = cvLoadImage("Resource/hand_color/testmode4.jpg", 1);
-    IplImage* src = cvLoadImage("Resource/hand_color/yellow.jpg", 1);
+    IplImage* src = cvLoadImage("Resource/hand_color/red_pen.jpg", 1);
+    // IplImage* src = cvLoadImage("Resource/hand_color/yellow.jpg", 1);
     cvAvgSdv(src, &MeanScalar, &StandardDeviationScalar);
 
     // cout << "src:" << src << endl;
@@ -426,24 +470,25 @@ bool DetectHandShakeSpeed(){
         preHandBitClock = clock();
         //////////////////////////////////// clock
     }
-    else if(pre_handmoveup != handmoveup && pre_handmoveup == false){
-        shaklowest = nowy;
-        shakrange = abs(shakhighest - shaklowest);
-        // cout << "!!!!!!!!!!!!!!!!!!!!!!!!!!!!Hand shakrange" << shakrange << endl;
-        compute_volume_M[a] = shakrange;
-        sum_volume = 0;
-        for(int i = 0; i < 6; i ++ ){
-            sum_volume = sum_volume + compute_volume_M[i];
-        }
+    // else if(pre_handmoveup != handmoveup && pre_handmoveup == false){
+    //     shaklowest = nowy;
+    //     shakrange = abs(shakhighest - shaklowest);
+    //     // cout << "!!!!!!!!!!!!!!!!!!!!!!!!!!!!Hand shakrange" << shakrange << endl;
+    //     compute_volume_M[go_buffer] = shakrange;
+    //     sum_volume = 0;
+    //     for(int i = 0; i < 6; i ++ ){
+    //         sum_volume = sum_volume + compute_volume_M[i];
+    //     }
 
-        average_volume = sum_volume / 6;
-        average_volume = 60 + (2 * average_volume);
-        // for(int i = 0; i < 6; i ++ )
-        //     cout << "compute_volume_M[" << i << "] " << compute_volume_M[i] << endl;
-        cout << "average_volume " << average_volume << endl;
-        if(average_volume < 127) volume = average_volume;
+    //     average_volume = sum_volume / 6;
+    //     average_volume = 60 + (2 * average_volume);
+    //     // for(int i = 0; i < 6; i ++ )
+    //     //     cout << "compute_volume_M[" << i << "] " << compute_volume_M[i] << endl;
+    //     // cout << "average_volume " << average_volume << endl;
+    //     if(average_volume < 127) volume = average_volume;
+    //     // cout << "change volume:" << volume << endl;
 
-    }
+    // }
 
     pre_handmoveup = handmoveup;
     int NOWTIME = clock();
