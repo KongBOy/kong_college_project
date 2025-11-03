1218be2bdac686284cc0d103402ab65f897adcda
KongBoy <s89334roy@yahoo.com.tw>
2025-11-03 17:29:20 +0800

    ScreenTool 排版

--- src/ScreenTool.cpp
+++ src/ScreenTool.cpp
@@ -5,25 +5,22 @@
 
 using namespace cv;
 
-void FadeInOut(string Title, Mat Now,Mat After,int delay){
-
+void FadeInOut(string Title, Mat Now, Mat After, int delay){
     int row=Now.rows;
     int col=Now.cols;
     int a;
-    imshow(Title,Now);
-    for(int k=10;k>0;k--){
-        for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
-                a=i*col+j;
-                if(a%k==0){
-                    Now.at<Vec3b>(i,j)= After.at<Vec3b>(i,j);
+    imshow(Title, Now);
+    for(int k=10; k > 0; k--){
+        for(int i=0; i < row; i++){
+            for(int j=0; j < col; j++){
+                a = i * col + j;
+                if(a % k == 0){
+                    Now.at<Vec3b>(i, j)= After.at<Vec3b>(i, j);
                 }
             }
         }
-        imshow(Title,Now);
+        imshow(Title, Now);
         waitKey(delay);
     }
-    imshow(Title,After);
-
-
+    imshow(Title, After);
 }
